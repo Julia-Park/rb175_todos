05f2c7c26a485eedffbd762187ecd0c9c94da074
Julia-Park <julia.ay.park@gmail.com>
2021-07-28 14:53:20 -0400

    Add ability to create new lists

--- todo.rb
+++ todo.rb
@@ -1,15 +1,31 @@
 require "sinatra"
-require "sinatra/reloader" if production?
+require "sinatra/reloader"
 require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  # use this block to make sure the user session contains at least an empty array
+  session[:lists] ||= []
+end
 
 get "/" do
   redirect "/lists"
 end
 
 get "/lists" do
-  @lists = [
-    { name: "Lunch Groceries", todos: [] },
-    { name: "Dinner Groceries", todos: [] }
-  ]
+  @lists = session[:lists]
   erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect "/lists"
 end
