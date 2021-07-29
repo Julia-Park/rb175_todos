b3e60be95a05ea00fbf45e29cb1cd8b4a854c843
Julia-Park <julia.ay.park@gmail.com>
2021-07-29 16:30:12 -0400

    Rubocop fixes

--- todo.rb
+++ todo.rb
@@ -1,6 +1,8 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -15,35 +17,35 @@ end
 helpers do
   def error_for_list_name(name)
     if !(1..100).cover?(name.length)
-      "The list name must be between 1 and 100 characters."
+      'The list name must be between 1 and 100 characters.'
     elsif session[:lists].any? { |list| list[:name].downcase == name.downcase }
-      "The list name must be unique."
+      'The list name must be unique.'
     end
   end
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
-get "/lists" do # view all lists
+get '/lists' do # view all lists
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
-get "/lists/new" do # renders the new list form
+get '/lists/new' do # renders the new list form
   erb :new_list, layout: :layout
 end
 
-post "/lists" do # create a new list
+post '/lists' do # create a new list
   list_name = params[:list_name].strip
+  session[:error] = error_for_list_name(list_name)
 
-  if error = error_for_list_name(list_name)
-    session[:error] = error
+  if session[:error]
     erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: []}
-    session[:success] = "The list has been created."
-    redirect "/lists"
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
   end
 end
